659913e814e46af0d5844b58899eecc1f38e4e10
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-04-17 14:50:28 +0200

    adding theme

---
+++ lib/shared/components/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// Main theme for light mode
+ThemeData lightTheme = ThemeData(
+  // Colors
+  brightness: Brightness.light,
+  primaryColor: Colors.lightBlue[800],
+    accentColor: Colors.cyan[600],
+);
