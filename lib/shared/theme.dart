61be3ee1dfa6e1d379ceb3a864c5e3c01b9a9860
Munyaradzi Chigangawa <56768581+Munyaradzi-Chigangawa@users.noreply.github.com>
2021-04-18 09:53:16 +0200

    Delete theme.dart

--- lib/shared/theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-// Main theme for light mode
-ThemeData lightTheme = ThemeData(
-  // Colors
-  brightness: Brightness.light,
-  primaryColor: Colors.lightBlue[800],
-    accentColor: Colors.cyan[600],
-);
