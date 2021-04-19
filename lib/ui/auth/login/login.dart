551b60fc719451d3189fbe534a2db22032968e8b
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-04-19 14:53:47 +0200

    login

---
+++ lib/ui/auth/login/login.dart
@@ -0,0 +1,12 @@
+  
+import 'package:city_mall/ui/auth/login/components/body.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
