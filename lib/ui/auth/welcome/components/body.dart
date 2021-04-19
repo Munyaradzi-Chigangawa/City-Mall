375d6775818edc8cfb3826d5067493fbd69d7d3b
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-04-19 14:57:21 +0200

    welcome screen body

---
+++ lib/ui/auth/welcome/components/body.dart
@@ -0,0 +1,62 @@
+import 'package:city_mall/shared/components/components/constants.dart';
+import 'package:city_mall/ui/auth/signup/signup.dart';
+import 'package:city_mall/ui/auth/welcome/components/background.dart';
+import 'package:flutter/material.dart';
+import 'package:city_mall/shared/components/components/rounded_button.dart';
+import 'package:city_mall/ui/auth/login/login.dart';
+import 'package:flutter_svg/svg.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    // This size provide us total height and width of our screen
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "WELCOME TO CITY MALL",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.05),
+            SvgPicture.asset(
+              "assets/icons/chat.svg",
+              height: size.height * 0.45,
+            ),
+            SizedBox(height: size.height * 0.05),
+            RoundedButton(
+              text: "LOGIN",
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+            RoundedButton(
+              text: "SIGN UP",
+              color: kPrimaryLightColor,
+              textColor: Colors.black,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return SignUpScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
